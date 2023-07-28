ecc6eeb5399438e59d924113f862a3168a1b5d5c
Immaculatekimani <immaculatewanjira21@gmail.com>
2023-07-28 11:42:23 +0300

    used the use effect function to get latest block

--- src/index.js
+++ src/index.js
@@ -18,7 +18,21 @@ ReactDOM.render(
   <Route path="/address/:id" element={<Address />} />
   <Route path="/blockTransactions/:id" element={<BlockTransactions />} />
 </routes>
-
+useEffect(()=>{
+  let blockArray = [];
+  let latestBlock = [];
+  const getLatestBlock = async () =>{
+    const blockNumber = alchemy.core.getBlockNumber();
+    setBlockNumber(blockNumber);
+    for(let i=0; i<=15;i++){
+      let block = alchemy.core.getBlock(blockNumber -1);
+      blockArray.push(block);
+    }
+    setLatestBlocks(blockArray);
+    console.log("latest block: "+ latestBlock);
+  }
+  getLatestBlock();
+}, [])
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
